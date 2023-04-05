6b8a07f1af80ac5c5253929335f981ee4b3027e9
Exz0tikWaffle <leotrem2567@gmail.com>
2023-04-05 19:22:35 -0400

    généré les fichiers .jar

---
+++ src/main/java/client/GUI/ClientApp.java
@@ -0,0 +1,14 @@
+package main.java.client.GUI;
+
+/**
+ * Cette classe existe seulement pour que le .jar du client graphique marche bien.
+ */
+public class ClientApp {
+    /**
+     * Fonction main qui démarre le client
+     * @param args arguments de la ligne de commande
+     */
+    public static void main(String[] args) {
+        GUI.main(args);
+    }
+}
